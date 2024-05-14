1e42162673aa8be997a43f50d40fe2ed4167c53d
John Odhiambo <theyohancraft@gmail.com>
2024-05-14 20:29:52 +0300

    Jwt security with jwt

---
+++ src/main/java/com/johanna/jwtsecurity/auth/AuthService.java
@@ -0,0 +1,52 @@
+package com.johanna.jwtsecurity.auth;
+
+import com.johanna.jwtsecurity.config.JwtService;
+import com.johanna.jwtsecurity.user.Role;
+import com.johanna.jwtsecurity.user.User;
+import com.johanna.jwtsecurity.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstName())
+                .lastname(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/johanna/jwtsecurity/user/Role.java
@@ -0,0 +1,6 @@
+package com.johanna.jwtsecurity.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
